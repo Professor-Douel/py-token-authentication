dfa701cc22400577da718136c971e288617ddc22
Oleksandr Barabanov <imtence@icloud.com>
2025-02-14 13:01:15 +0200

    Solution

---
+++ cinema/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminOrAuthenticatedReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS and request.user.is_authenticated:
+            return True
+        return request.user.is_authenticated and request.user.is_staff
--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,21 @@
-# write your code here
+from django.urls import path
+
+from user.views import (
+    UserCreateView,
+    TokenCreateView,
+    UserManagingView
+)
+
+urlpatterns = [
+    path(
+        "register/", UserCreateView.as_view(), name="create"
+    ),
+    path(
+        "login/", TokenCreateView.as_view(), name="login"
+    ),
+    path(
+        "me/", UserManagingView.as_view(), name="manage"
+    )
+]
+
+app_name = "user"
